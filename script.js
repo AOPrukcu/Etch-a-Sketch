a78df038c0fed0ed65a83543fdaf5dd191a271ad
AO <103045021+AOPrukcu@users.noreply.github.com>
2024-10-10 21:59:55 +0300

    finished project

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const container = document.createElement("div");
-container.classList.add("container")
-window.addEventListener("load",addSquares(16))
-
+container.classList.add("container");
+window.addEventListener("load", addSquares(16));
 
 body = document.querySelector("body");
-
 body.appendChild(container);
 
 const addBtn = document.createElement("button");
 addBtn.textContent = "Sıfırla";
 body.appendChild(addBtn);
-
 
 addBtn.addEventListener("click", () => {
   let num = +window.prompt("lütfen kaç kare istiyorsanız girin");
@@ -18,11 +15,19 @@ addBtn.addEventListener("click", () => {
 });
 
 function addSquares(num) {
-  for (let i = 0; i < num*16; i++) {
-    const box = document.createElement("div")
+    container.replaceChildren();
+    for (let i = 0; i < num * num; i++) {
+    
+    const box = document.createElement("div");
     box.classList.add("box");
-    const boxNum = (960 / num*num)
-    // box.style.width = boxNum.toString() + "px";
+    box.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "black";
+    });
+    const squarewidth = 960 / num;
+
+    box.style.width = squarewidth.toString() + "px";
+    box.style.height = squarewidth.toString() + "px";
+
     container.appendChild(box);
   }
 }
